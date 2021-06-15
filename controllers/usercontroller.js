0ab04baaa11d941a0d070eb60b4a19a87b21d94b
Cherron Simes <cherron@developdbycherron.com>
2021-06-14 20:03:26 -0500

    Endpoint added to create user

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -4,5 +4,51 @@ const { User } = require("../models");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+router.post("/create", async (req, res) => {
+
+    let { username, password } = req.body.user;
+
+    try {
+
+    const newUser = await User.create({
+        username,
+        password
+    });
+
+    res.status(200).json({
+            message: "User successfully created!",
+            user: newUser
+        });
+
+    } catch (err) {
+        res.status(500).json({ error }) 
+    }
+});
+
+router.post("/login", async (req, res) => {
+    let { username, password } = req.body.user;
+
+    try {
+        const loggedInUser = await User.findOne({
+            where : {
+                username: username,
+                password: password
+            }
+        })
+        if(loggedInUser) {
+            res.status(200).json({
+                message: "User successfully logged in!",
+                user: loggedInUser
+            })
+        } else {
+            res.status(401).json({
+                message: "Login failed."
+            })
+        }
+    } catch (err) {
+        res.status(500).json({ Error: err })
+    }
+});
+
 
 module.exports = router;
--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const {Sequelize} = require('sequelize');
 
-const db = new Sequelize("postgres://postgres:YOURPASSWORDHERE@localhost:5432/animal-server");
+const db = new Sequelize("postgres://postgres:97c2dd0705204ed4a3e3fb5ef76979ce@localhost:5432/animal-server");
 
 module.exports = db;
